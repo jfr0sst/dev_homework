eb957fb070a642c7cfac485e5c9091abd8ee816c
igorkudravcev <igor@rts-soft.ru>
2022-04-12 20:51:41 +0300

    add maven homework

---
+++ service/src/test/java/org/jfrost/servtest/AppServiceTest.java
@@ -0,0 +1,13 @@
+package org.jfrost.servtest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppServiceTest {
+
+    @Test
+    public void appUnitSercviceTest() {
+        assertTrue(true);
+    }
+}
