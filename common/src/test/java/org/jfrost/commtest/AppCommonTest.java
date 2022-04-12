eb8b8b22b25e672cd31be3903b66285090fe611c
igorkudravcev <igor@rts-soft.ru>
2022-04-12 20:51:41 +0300

    add maven homework

---
+++ common/src/test/java/org/jfrost/commtest/AppCommonTest.java
@@ -0,0 +1,13 @@
+package org.jfrost.commtest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppCommonTest {
+
+    @Test
+    public void appUnitCommonTest() {
+        assertTrue(true);
+    }
+}
---
+++ service/src/test/java/org/jfrost/servtest/inegration/ServiceTestIT.java
@@ -0,0 +1,14 @@
+package org.jfrost.servtest.inegration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ServiceTestIT {
+
+    @Test
+    public void appIntegrationServiceTest(){
+        assertTrue(true);
+    }
+
+}
